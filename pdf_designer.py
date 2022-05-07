f697bd12aa011480e16b37610c792a5b545b7f7e
Rifa <68515313+Rifamim1999@users.noreply.github.com>
2022-05-08 01:04:23 +0600

    Create pdf_designer.py
    https://www.hackerrank.com/challenges/designer-pdf-viewer/problem?isFullScreen=true&h_r=next-challenge&h_v=zen

---
+++ pdf_designer.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+    # Write your code here
+    letters =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    li=[]
+    for i in range(len(letters)):
+        for j in range(len(word)):
+            if word[j]==letters[i]:
+                li.append(h[i])
+    li.sort(reverse = True)
+    maxi=li[0]
+    a = maxi*len(word)
+    return a
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
